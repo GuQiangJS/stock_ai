1cba90d8f65b02ed324456929957e2671fcacd0a
Gu Qiang <GuQiangJS@users.noreply.github.com>
2019-05-10 13:31:47 +0800

    travis

---
+++ setup.py
@@ -0,0 +1,35 @@
+from setuptools import setup, find_packages
+
+import versioneer
+
+NAME = 'stock_ai'
+
+
+def readme():
+    with open('README.md', encoding='utf-8') as f:
+        return f.read()
+
+
+setup(
+    name=NAME,
+    version=versioneer.get_version(),
+    cmdclass=versioneer.get_cmdclass(),
+    description=readme(),
+    long_description=readme(),
+    author='GuQiangJS',
+    author_email='guqiangjs@gmail.com',
+    url='https://github.com/GuQiangJS/stock_ai.git',
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Environment :: Console',
+        'Intended Audience :: Science/Research',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 3.7',
+        'Topic :: Scientific/Engineering',
+    ],
+    keywords='data',
+    install_requires=['QUANTAXIS', 'TensorFlow'],
+    packages=find_packages(),
+    zip_safe=False,
+    exclude_package_data={'': ['test_*.py']}
+)
