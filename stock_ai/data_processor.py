8a950d544d8539b1c741a0c1b0ec904eaebbedd4
Gu Qiang <GuQiangJS@users.noreply.github.com>
2019-05-14 15:27:05 +0800

    数据读取基础

--- stock_ai/data_processor.py
+++ stock_ai/data_processor.py
@@ -61,11 +61,15 @@ def load_stock_daily(code, **kwargs):
 def load_stock_daily_online(code, start, end, columns, times=5):
     """读取股票在线日线数据
 
+    Notes:
+        在线数据只取tdx的数据。
+
     Args:
-        start: See parameter ``start`` in :func:`fetch_stock`.
-        end:
-        columns:
-        code (str): 股票代码。
+        code: 参考 :func:`load_stock_daily` 中参数 `start` 的说明。
+        start: 参考 :func:`load_stock_daily` 中参数 `start` 的说明。
+        end: 参考 :func:`load_stock_daily` 中参数 `start` 的说明。
+        fq: 参考 :func:`load_stock_daily` 中参数 `start` 的说明。
+        columns: 参考 :func:`load_stock_daily` 中参数 `start` 的说明。
         times (int, optional): 重试次数。默认为 `5`。
     Returns:
         :py:class:`pandas.DataFrame`: 股票日线数据。
@@ -94,6 +98,8 @@ def load_stock_daily_csv(path, **kwargs):
         encoding (str): 文件编码。默认为 `utf-8`。参考 :py:func:`pandas.read_csv` 中同名参数。
         parse_dates (str): 文件编码。默认为 `utf-8`。参考 :py:func:`pandas.read_csv` 中同名参数。
         float_precision (str): 浮点类型转换。默认为 `round_trip`。参考 :py:func:`pandas.read_csv` 中同名参数。
+    Returns:
+        :py:class:`pandas.DataFrame`: 股票日线数据。
     """
     index_col = kwargs.pop('index_col', 'date')
     encoding = kwargs.pop('encoding', _csv_encoding)
@@ -110,13 +116,14 @@ def load_stock_daily_mongodb(code, start, end, fq, columns):
     """读取股票本地日线数据
 
     Args:
-        code:
-        start:
-        end:
-        fq:
-        columns:
+        code: 参考 :func:`load_stock_daily` 中参数 `start` 的说明。
+        start: 参考 :func:`load_stock_daily` 中参数 `start` 的说明。
+        end: 参考 :func:`load_stock_daily` 中参数 `start` 的说明。
+        fq: 参考 :func:`load_stock_daily` 中参数 `start` 的说明。
+        columns: 参考 :func:`load_stock_daily` 中参数 `start` 的说明。
 
     Returns:
+        :py:class:`pandas.DataFrame`: 股票日线数据。
 
     """
     d = QA.QA_fetch_stock_day_adv(code, start=start, end=end)
@@ -131,3 +138,299 @@ def load_stock_daily_mongodb(code, start, end, fq, columns):
     # 原始列名['open', 'high', 'low', 'close', 'volume', 'amount', 'preclose', 'adj']
     df = df.reset_index().drop(columns=['code']).set_index('date')
     return df[columns]
+
+
+def load_index_daily(code, **kwargs):
+    """读取指数日线数据（在线或从数据库）
+
+        Args:
+            code (str): 指数代码。
+            start (str, optional): 开始日期。数据格式为%Y-%m-%d。默认为`1990-01-01`。
+            end (str, optional): 结束日期。数据格式为%Y-%m-%d。默认为`当天`。
+            online (bool, optional): 是否获取在线数据。默认为 `False`。
+            columns (tuple, optional): 获取的列集合。默认为
+                ['open', 'high', 'low', 'close', 'up_count', 'down_count', 'volume','amount']
+
+        Returns:
+            :py:class:`pandas.DataFrame`: 股票日线数据。
+
+        """
+    start = kwargs.pop('start', '1990-01-01')
+    end = kwargs.pop('end', util.date2str(datetime.datetime.now()))
+    online = kwargs.pop('online', False)
+    columns = kwargs.pop('columns', [
+        'open', 'high', 'low', 'close', 'up_count', 'down_count', 'volume',
+        'amount'
+    ])
+    if online:
+        return load_index_daily_online(code, start, end, columns)
+    else:
+        return load_index_daily_mongodb(code, start, end, columns)
+
+
+def load_index_daily_online(code, start, end, columns, times=5):
+    """读取指数在线日线数据
+
+    Args:
+        start (str):  参考 :func:`load_index_daily` 中参数 `start` 的说明。
+        end (str): 参考 :func:`load_index_daily` 中参数 `end` 的说明。
+        columns: 参考 :func:`load_index_daily` 中参数 `columns` 的说明。原始的列名中volume是vol，为了统一，内部会修改为volume。
+        code (str): 参考 :func:`load_index_daily` 中参数 `code` 的说明。
+        times (int, optional): 重试次数。默认为 `5`。
+    Returns:
+        :py:class:`pandas.DataFrame`: 股票日线数据。
+    """
+    retries = 0
+    while True:
+        try:
+            df = QA.QAFetch.QATdx.QA_fetch_get_index_day(code, start, end)
+            # 原始列名['open', 'close', 'high', 'low', 'vol', 'amount', 'up_count', 'down_count', 'date', 'code', 'date_stamp'] pylint: disable=C0301
+            df = df.rename(columns={'vol': 'volume'})[columns]
+            return df
+        except socket.timeout:
+            if retries < times:
+                retries = retries + 1
+                continue
+            raise
+
+
+def load_index_daily_mongodb(code, start, end, columns):
+    """读取指数本地日线数据
+
+    Args:
+        start (str):  参考 :func:`load_index_daily` 中参数 `start` 的说明。
+        end (str): 参考 :func:`load_index_daily` 中参数 `end` 的说明。
+        columns: 参考 :func:`load_index_daily` 中参数 `columns` 的说明。
+        code (str): 参考 :func:`load_index_daily` 中参数 `code` 的说明。
+    Returns:
+        :py:class:`pandas.DataFrame`: 指数日线数据。
+    """
+    d = QA.QA_fetch_index_day_adv(code, start, end)
+    df = d.data.reset_index().drop(columns=['code']).set_index('date')
+    return df[columns]
+
+
+def load_stock_list(online=False):
+    """获取股票列表
+
+    Args:
+        online (bool): 是否获取在线数据。默认为False。
+    """
+    return load_stock_list_online() if online else load_stock_list_mongodb()
+
+
+def load_stock_list_online():
+    """从在线数据中获取股票列表。
+
+    Notes:
+        在线数据只取tdx的数据。
+
+    Returns:
+        :py:class:`pandas.DataFrame`: 股票列表。
+
+    Examples:
+
+        >>> data_processor.load_stock_list_mongodb().head()
+                    code  decimal_point  name    pre_close       sec sse  volunit
+            code
+            000001  000001              2  平安银行   648.020000  stock_cn  sz      100
+            000002  000002              2  万 科Ａ    25.410000  stock_cn  sz      100
+            000004  000004              2  国农科技    16.299999  stock_cn  sz      100
+            000005  000005              2  世纪星源  3778.008125  stock_cn  sz      100
+            000006  000006              2  深振业Ａ     5.270000  stock_cn  sz      100
+
+    """
+    df = QA.QAFetch.QATdx.QA_fetch_get_stock_list()
+    df.index = df.index.droplevel(1)
+    return df
+
+
+def load_stock_list_mongodb():
+    """从本地数据库中获取股票列表。
+
+    Returns:
+        :py:class:`pandas.DataFrame`: 股票列表。
+
+    Examples:
+
+        >>> data_processor.load_stock_list_mongodb().head()
+                      code  volunit  decimal_point  name    pre_close sse       sec
+        code   sse
+        000001 sz   000001      100              2  平安银行  1096.050000  sz  stock_cn
+        000002 sz   000002      100              2  万 科Ａ    28.049999  sz  stock_cn
+        000004 sz   000004      100              2  国农科技    21.410000  sz  stock_cn
+        000005 sz   000005      100              2  世纪星源  4738.003750  sz  stock_cn
+        000006 sz   000006      100              2  深振业Ａ     5.680000  sz  stock_cn
+
+    """
+    return QA.QA_fetch_stock_list_adv()
+
+
+def load_stock_info_mongodb(code):
+    """从数据库中获取股票信息
+
+    Args:
+        code (str): 股票代码
+
+    Examples:
+        >>> data_processor.load_stock_info_mongodb('601398').dtypes
+        baoliu2               float64
+        bgu                   float64
+        changqifuzhai         float64
+        code                   object
+        cunhuo                float64
+        faqirenfarengu        float64
+        farengu               float64
+        gudingzichan          float64
+        gudongrenshu          float64
+        guojiagu              float64
+        hgu                   float64
+        industry                int64
+        ipo_date                int64
+        jinglirun             float64
+        jingyingxianjinliu    float64
+        jingzichan            float64
+        lirunzonghe           float64
+        liudongfuzhai         float64
+        liudongzichan         float64
+        liutongguben          float64
+        market                  int64
+        meigujingzichan       float64
+        province                int64
+        shuihoulirun          float64
+        touzishouyu           float64
+        updated_date            int64
+        weifenpeilirun        float64
+        wuxingzichan          float64
+        yingshouzhangkuan     float64
+        yingyelirun           float64
+        zhigonggu             float64
+        zhuyinglirun          float64
+        zhuyingshouru         float64
+        zibengongjijin        float64
+        zongguben             float64
+        zongxianjinliu        float64
+        zongzichan            float64
+        dtype: object
+
+    Returns:
+        :py:class:`pandas.DataFrame`: 股票信息。
+
+    """
+    return QA.QA_fetch_stock_info(code)
+
+
+def load_stock_info_online(code):
+    """在线获取股票信息
+
+    Notes:
+        在线数据只取tdx的数据。
+
+    Args:
+        code (str): 股票代码
+
+    Examples:
+        >>> data_processor.load_stock_info_online('601398').dtypes
+        market                  int64
+        code                   object
+        liutongguben          float64
+        province                int64
+        industry                int64
+        updated_date            int64
+        ipo_date                int64
+        zongguben             float64
+        guojiagu              float64
+        faqirenfarengu        float64
+        farengu               float64
+        bgu                   float64
+        hgu                   float64
+        zhigonggu             float64
+        zongzichan            float64
+        liudongzichan         float64
+        gudingzichan          float64
+        wuxingzichan          float64
+        gudongrenshu          float64
+        liudongfuzhai         float64
+        changqifuzhai         float64
+        zibengongjijin        float64
+        jingzichan            float64
+        zhuyingshouru         float64
+        zhuyinglirun          float64
+        yingshouzhangkuan     float64
+        yingyelirun           float64
+        touzishouyu           float64
+        jingyingxianjinliu    float64
+        zongxianjinliu        float64
+        cunhuo                float64
+        lirunzonghe           float64
+        shuihoulirun          float64
+        jinglirun             float64
+        weifenpeilirun        float64
+        meigujingzichan       float64
+        baoliu2               float64
+        dtype: object
+
+    Returns:
+        :py:class:`pandas.DataFrame`: 股票信息。
+
+    """
+    return QA.QAFetch.QATdx.QA_fetch_get_stock_info(code)
+
+
+def load_stock_block_mongodb():
+    """从数据库获取股票板块信息
+
+    Examples:
+
+        >>> df = data_processor.load_stock_block_mongodb().loc['601398'].head()
+        >>> df
+               blockname    code type source
+        code
+        601398       环渤海  601398   gn    tdx
+        601398       含H股  601398   gn    tdx
+        601398     沪深300  601398   yb    tdx
+        601398      融资融券  601398   yb    tdx
+        601398       环渤海  601398   yb    tdx
+
+        >>> d1 = df.loc['601398']
+        >>> set(d1['type'].values)
+        {'yb', 'zs', 'thshy', 'dy', 'zjhhy', 'fg', 'gn'}
+        >>> set(d1['source'].values)
+        {'tdx', 'ths'}
+
+    Returns:
+        :py:class:`pandas.DataFrame`: 股票板块信息。
+
+    """
+    return QA.QA_fetch_stock_block()
+
+
+def load_stock_block_online():
+    """在线获取股票板块信息
+
+    Notes:
+        在线数据只取tdx的数据。
+
+    Examples:
+
+        >>> df = data_processor.load_stock_block_online().loc['601398'].head()
+        >>> df
+               blockname    code type source
+        code
+        601398       环渤海  601398   gn    tdx
+        601398       含H股  601398   gn    tdx
+        601398     沪深300  601398   yb    tdx
+        601398      融资融券  601398   yb    tdx
+        601398       环渤海  601398   yb    tdx
+
+        >>> d1 = df.loc['601398']
+        >>> set(d1['type'].values)
+        {{'gn', 'zs', 'fg', 'yb'}
+        >>> set(d1['source'].values)
+        {'tdx'}
+
+    Returns:
+        :py:class:`pandas.DataFrame`: 股票板块信息。
+
+    """
+    return QA.QAFetch.QATdx.QA_fetch_get_stock_block()
--- test/test_data_processor.py
+++ test/test_data_processor.py
@@ -1,6 +1,7 @@
 import test
 from stock_ai import data_processor
-from pandas.testing import assert_frame_equal
+import pytest
+import os
 
 
 def test_load_stock_daily_csv():
@@ -21,8 +22,77 @@ def test_load_stock_daily_online():
                                                         '1990-01-01',
                                                         end=end,
                                                         columns=online_columns)
+    assert df_online1 is not None
     assert not df_online1.empty
     df_online2 = data_processor.load_stock_daily(code, end=end, online=True)
+    assert df_online2 is not None
     assert not df_online2.empty
     assert df_online1.equals(df_online2)
-    assert assert_frame_equal(df_online1, df_online2)
+
+
+@pytest.mark.skipif("TRAVIS" in os.environ and os.environ["TRAVIS"] == "true",
+                    reason="Skipping this test on Travis CI.")
+def test_load_stock_list_mongo():
+    """测试从数据库中读取股票列表"""
+    df = data_processor.load_stock_list_mongodb()
+    assert not df.empty
+    for name in [
+            'code', 'decimal_point', 'name', 'pre_close', 'sec', 'sse',
+            'volunit'
+    ]:
+        assert name in df.columns
+
+
+def test_load_stock_list_online():
+    """测试在线数据中读取股票列表"""
+    df = data_processor.load_stock_list_online()
+    print(df.head())
+    assert not df.empty
+    for name in [
+            'code', 'decimal_point', 'name', 'pre_close', 'sec', 'sse',
+            'volunit'
+    ]:
+        assert name in df.columns
+
+
+@pytest.mark.skipif("TRAVIS" in os.environ and os.environ["TRAVIS"] == "true",
+                    reason="Skipping this test on Travis CI.")
+def test_load_stock_list():
+    df1 = data_processor.load_stock_list_mongodb()
+    df2 = data_processor.load_stock_list(online=False)
+    df1.equals(df2)
+    df1 = data_processor.load_stock_list_online()
+    df2 = data_processor.load_stock_list(online=True)
+    df1.equals(df2)
+
+
+def test_load_stock_info_online():
+    code = '601398'
+    df = data_processor.load_stock_info_online(code)
+    print(df.dtypes)
+
+
+@pytest.mark.skipif("TRAVIS" in os.environ and os.environ["TRAVIS"] == "true",
+                    reason="Skipping this test on Travis CI.")
+def test_load_stock_info_mongo():
+    code = '601398'
+    df = data_processor.load_stock_info_mongodb(code)
+    print(df.dtypes)
+
+
+def test_load_stock_block_online():
+    df = data_processor.load_stock_block_online()
+    d1 = df.loc['601398']
+    print(set(d1['type'].values))
+    print(set(d1['source'].values))
+    print(df.loc['601398'].head())
+
+
+@pytest.mark.skipif("TRAVIS" in os.environ and os.environ["TRAVIS"] == "true",
+                    reason="Skipping this test on Travis CI.")
+def test_load_stock_block_mongodb():
+    df = data_processor.load_stock_block_mongodb()
+    d1 = df.loc['601398']
+    print(set(d1['type'].values))
+    print(set(d1['source'].values))
+    print(df.loc['601398'].head())
