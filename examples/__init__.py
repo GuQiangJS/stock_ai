999f29e1ab5ff08ac80918158211b69091b52256
Gu Qiang <GuQiangJS@users.noreply.github.com>
2019-08-26 09:12:57 +0800

    20190826

--- examples/__init__.py
+++ examples/__init__.py
@@ -30,7 +30,7 @@ def get_index_daily(code: str = index_code) -> pd.DataFrame:
     if __cache and code in __cache:
         return __cache[code]
     df = data_processor.load_index_daily(code, online=is_travis)
-    df=df.drop(columns=['down_count','up_count'])
+    df = df.drop(columns=['down_count', 'up_count'])
     logging.debug("Load Daily:" + code)
     __cache[code] = df
     return df
@@ -45,6 +45,7 @@ def get_data():
     df.dropna(inplace=True)
     return df
 
+
 def get_data_regression(y_col='close601398', **kwargs):
     """回归测试使用的数据源"""
     test_split = kwargs.pop('test_split', 0.1)
@@ -53,7 +54,7 @@ def get_data_regression(y_col='close601398', **kwargs):
     df = get_data()
 
     # cols_X=[col for col in df.columns if col != y_col and '601398' in col]
-    cols_X=['open601398', 'high601398', 'low601398','close601398']
+    cols_X = ['open601398', 'high601398', 'low601398', 'close601398']
     print(cols_X)
     x_np = df[cols_X].to_numpy()
 
@@ -62,22 +63,24 @@ def get_data_regression(y_col='close601398', **kwargs):
     round_Y = kwargs.pop('round_Y', 4)
     y_np = np.round(y_np, round_Y)
 
-    length = kwargs.pop('length', 5)#x轴天数
+    length = kwargs.pop('length', 5)  #x轴天数
     sampling_rate = kwargs.pop('sampling_rate', 1)
     batch_size = kwargs.pop('batch_size', 1)
 
-    data_gen = TimeseriesGenerator(x_np, y_np,
-                                   length=length, sampling_rate=sampling_rate,
+    data_gen = TimeseriesGenerator(x_np,
+                                   y_np,
+                                   length=length,
+                                   sampling_rate=sampling_rate,
                                    batch_size=batch_size)
 
     x = []
     y = []
 
-    round_X=kwargs.pop('round_Y', 4)
+    round_X = kwargs.pop('round_Y', 4)
     scaler = kwargs.pop('scaler', sklearn.preprocessing.MinMaxScaler())
 
     for d in data_gen:
-        x.append(np.round(d[0][0],round_X))
+        x.append(np.round(d[0][0], round_X))
         y.append(d[1][0])
 
     x_train = np.array(x[:int(len(x) * (1 - test_split))])
@@ -85,6 +88,7 @@ def get_data_regression(y_col='close601398', **kwargs):
     x_test = np.array(x[int(len(x) * (1 - test_split)):])
     y_test = np.array(y[int(len(x) * (1 - test_split)):])
     return (x_train, y_train), (x_test, y_test)
+
 
 def get_data_classification(y_col='close601398', **kwargs):
     """分类测试使用的数据源"""
@@ -102,13 +106,22 @@ def get_data_classification(y_col='close601398', **kwargs):
     round_Y = kwargs.pop('round_Y', 2)
     y_np = np.round(y_np, round_Y)
 
+    skip_Y = kwargs.pop('skip_Y', 0)
+
+    if skip_Y > 0:
+        x_np = x_np[:-skip_Y]
+        y_np = y_np[skip_Y:]
+
     length = kwargs.pop('length', 5)
     sampling_rate = kwargs.pop('sampling_rate', 1)
     batch_size = kwargs.pop('batch_size', 1)
 
-    data_gen = TimeseriesGenerator(x_np, y_np,
-                                   length=length, sampling_rate=sampling_rate,
-                                   batch_size=batch_size)
+    data_gen = TimeseriesGenerator(x_np,
+                                   y_np,
+                                   length=length,
+                                   sampling_rate=sampling_rate,
+                                   batch_size=batch_size,
+                                   shuffle=kwargs.pop('shuffle', True))
 
     x = []
     y = []
@@ -128,4 +141,3 @@ def get_data_classification(y_col='close601398', **kwargs):
     x_test = np.array(x[int(len(x) * (1 - test_split)):])
     y_test = np.array(y[int(len(x) * (1 - test_split)):])
     return (x_train, y_train), (x_test, y_test)
-
